16f8da07c25ef7ed96cbdc1f08ece7f0dfb99a27
leehoseo <leehoseo@game.hoseo.edu>
2016-09-25 22:50:37 +0900

    Rock, Firre script 만듬

---
+++ Wolf/Assets/Scripts/Fire.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if("Rock" == collision.gameObject.tag)
+        {
+            Debug.Log("dsfsdf");
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Wolf/Assets/Scripts/Rock.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rock : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(true == this.GetComponent<Rigidbody2D>().isKinematic)
+            this.GetComponent<Rigidbody2D>().isKinematic = false;
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            this.GetComponent<Rigidbody2D>().isKinematic = true;
+        }
+   
+    }
+}
