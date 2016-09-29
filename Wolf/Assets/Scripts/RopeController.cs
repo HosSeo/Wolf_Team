50d9cad77600e5ba55dba1fd2d4d0c0892e8d82b
leehoseo <leehoseo@game.hoseo.edu>
2016-09-29 22:28:36 +0900

    Prefab 생성 저울 관련 스크립트 작성중

---
+++ Wolf/Assets/Scripts/RopeController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RopeController : MonoBehaviour {
+
+    // Use this for initialization
+    [SerializeField]
+    private LineRenderer rope;
+    
+    [SerializeField]
+    private float pulleyToRope;
+    [SerializeField]
+    private float positionControl;
+    [SerializeField]
+    private float ceilingHight;
+
+    
+
+	void Start () {
+        Vector2 ceilingPosition;
+        ceilingPosition.x = this.transform.position.x + this.transform.localScale.x + positionControl;
+        ceilingPosition.y = ceilingHight;
+
+        rope.SetPosition(0, ceilingPosition );
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ropeControl();
+
+    }
+
+    void ropeControl()
+    {
+        Vector2 ropePosition;
+        ropePosition.x = this.transform.position.x + this.transform.localScale.x + positionControl;
+        ropePosition.y = this.transform.position.y + pulleyToRope;
+
+        rope.SetPosition(1, ropePosition);
+    }
+}
---
+++ Wolf/Assets/Scripts/Scales.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scales : MonoBehaviour {
+
+    // Use this for initialization
+
+    [SerializeField]
+    private GameObject pulley_1 , pulley_2;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
